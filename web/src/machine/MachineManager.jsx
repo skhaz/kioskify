49bb1b8a9fd1e92934450902d4b18588a3e3fd37
Rodrigo Delduca <rodrigodelduca@gmail.com>
2019-04-17 19:37:51 -0300

    Bump

--- web/src/machine/MachineManager.jsx
+++ web/src/machine/MachineManager.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { makeStyles } from '@material-ui/styles'
 import Paper from '@material-ui/core/Paper'
+import List from '@material-ui/core/List'
 import AddIcon from '@material-ui/icons/Add'
 import Fab from '@material-ui/core/Fab'
 import firebase from '../helpers/firebase'
@@ -16,6 +17,13 @@ const useStyles = makeStyles(theme => ({
     position: 'absolute',
     bottom: theme.spacing.unit * 2,
     right: theme.spacing.unit * 2
+  },
+
+  list: {
+    overflowY: 'auto',
+    overflowX: 'hidden',
+    height: 'calc(100vh - 96pt)',
+    backgroundColor: '#f8f8f8'
   }
 }))
 
@@ -27,6 +35,12 @@ export default () => {
 
   return (
     <Paper className={classes.paper}>
+      <List
+        dense
+        disablePadding
+        className={classes.list}
+      >
+      </List>
       <Fab
         color='secondary'
         className={classes.fab}
